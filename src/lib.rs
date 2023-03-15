59ee3d6edfc7faac9cad08acd16945bd04af0481
Piotr Sarna <sarna@scylladb.com>
2023-03-15 09:04:33 +0100

    Step 3: add communication with the database

--- src/lib.rs
+++ src/lib.rs
@@ -29,7 +29,39 @@ pub async fn main(req: Request, env: Env, _ctx: worker::Context) -> Result<Respo
     // functionality and a `RouteContext` which you can use to  and get route parameters and
     // Environment bindings like KV Stores, Durable Objects, Secrets, and Variables.
     router
-        .get("/", |_, _| Response::ok("Hello from Workers!"))
+        .get_async("/", |_, ctx| async move {
+            use libsql_client::{workers::Connection as DbConnection, Connection, ResultSet};
+            use std::convert::TryFrom;
+
+            // Set up the database connection. connect_from_ctx assumes that you put the following variables
+            // in .dev.vars or Cloudflare Workers secrets (wrangler secret put (...)):
+            //   LIBSQL_CLIENT_URL=https://your-db-name.turso.io # remember about https:// or http://! It needs to be specified
+            //   LIBSQL_CLIENT_TOKEN=<your token obtained via `turso db auth token` command>
+            let connection = match DbConnection::connect_from_ctx(&ctx) {
+                Ok(conn) => conn,
+                Err(e) => return Response::error(e.to_string(), 400),
+            };
+
+            // Execute a database query - select a random number. We're not inserting anything
+            // to the database this time, but execute() can work that way too, just send an INSERT or UPDATE statement.
+            let result_set = match connection.execute("SELECT random() AS lucky_number").await {
+                Ok(result) => result.into_result_set(),
+                Err(e) => return Response::error(e.to_string(), 400),
+            };
+
+            // Extract the query result - we're expecting a single number to be returned under the column "lucky_number"
+            let lucky_number = match result_set {
+                Ok(ResultSet { columns: _, rows }) => {
+                    let value = &rows.first().expect("expected one row").cells["lucky_number"];
+                    i64::try_from(value.clone()).unwrap_or_default()
+                }
+
+                Err(e) => return Response::error(e.to_string(), 400),
+            };
+
+            // Generate the successful response via Workers API
+            Response::ok(format!("Your lucky number is: {lucky_number}"))
+        })
         .post_async("/form/:field", |mut req, ctx| async move {
             if let Some(name) = ctx.param("field") {
                 let form = req.form_data().await?;
